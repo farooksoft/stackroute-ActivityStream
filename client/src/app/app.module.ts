81fde218150429a21209682c4ee336fa24eeff3d
farooksoft <farooksoft@gmail.com>
2017-05-30 13:32:16 +0530

    circles and Dashboard rewamped

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AlertService, AuthenticationService, UserService, CircleService } from 
 import { HomeComponent } from './home/index';
 import { LoginComponent } from './login/index';
 import { RegisterComponent } from './register/index';
+import { CircleComponent } from './circle/index';
 
 @NgModule({
     imports: [
@@ -30,7 +31,8 @@ import { RegisterComponent } from './register/index';
         AlertComponent,
         HomeComponent,
         LoginComponent,
-        RegisterComponent
+        RegisterComponent,
+        CircleComponent
     ],
     providers: [
         AuthGuard,
---
+++ client/src/app/circle/circle.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+    moduleId: module.id.toString(),
+    selector: 'circle',
+    templateUrl: 'circle.component.html',
+})
+
+export class CircleComponent implements OnInit{
+    
+}
